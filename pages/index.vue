7300bcea0f92b57f34e29d4eed9b3fb052fe28d9
Aldwin <elonyx7@gmail.com>
2025-05-13 14:22:52 +0200

    reinstall to clean project

--- pages/index.vue
+++ pages/index.vue
@@ -9,8 +9,8 @@
   </div>
   <div class="rounded-2xl bg-gray-100 flex justify-center items-center h-70 mt-5 w-100 mr-auto ml-auto">
     <ul>
-      <li v-for="transaction in transactions" :key="transaction.id">
-        {{ transaction.transactionType }} of {{ transaction.amount }}€ on {{ transaction.date }}
+      <li>
+
       </li>
     </ul>
   </div>
@@ -18,39 +18,21 @@
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
-import prisma from "~/lib/prisma";
-import { HistoryTransact } from '@prisma/client';
+const { $prisma } = useNuxtApp();
 
 const balance = ref(0);
 const items = ref([{label: 'Débit', value: 'debit'}, {label: 'Crédit', value: 'credit'}]);
 const amount = ref(0);
 const selectedType = ref('debit');
-const transactions = ref<HistoryTransact[]>([]);
+const transactions = ref(0);
 
-onMounted(async () => {
-  // Charger l'historique des transactions depuis la base de données
-  transactions.value = await prisma.historyTransact.findMany();
-});
 
 const applyTransaction = async () => {
-  const numericAmount = parseFloat(amount.value);
+  const numericAmount = amount.value;
   if (selectedType.value === 'debit') {
     balance.value -= numericAmount;
   } else if (selectedType.value === 'credit') {
     balance.value += numericAmount;
   }
-
-  // Enregistrer la transaction dans la base de données
-  const transaction = await prisma.historyTransact.create({
-    data: {
-      transactionType: selectedType.value,
-      amount: numericAmount,
-      date: new Date(),
-      description: `${selectedType.value} of ${numericAmount}€`,
-    },
-  });
-
-  // Ajouter la transaction à l'historique local
-  transactions.value.push(transaction);
 };
 </script>
